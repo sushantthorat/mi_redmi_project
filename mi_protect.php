aca304e6a1cb22be061bd4d77c8e04cfe922ad90
sushant thorat <sushant.thorat@ahytech.com>
2017-04-08 13:08:11 +0530

    protect damage section and warranty section added here

---
+++ mi_protect.php
@@ -0,0 +1,120 @@
+<?php require_once('header.php'); ?>
+<link rel="stylesheet" href="css/protectout.css">
+
+  <div class="mi_protect_bg_head">
+    <div class="container">
+      <div class="sec_head">
+        <h3 class="title">Mi Protect</h3>
+        <p class="descp">
+  			Mi Protect covers accidental &amp; liquid damage <br>
+  			Free home pickup &amp; drop, and free second servicing within 1 year from activation
+  		</p>
+      </div>
+
+      <div class="product_cost_list">
+        <ul class="product_list_info row">
+          <li class="col-sm-2">
+            <div class="pr_info">
+              <p class="price font-number"><span class="small">₹ </span>449</p>
+              <p class="pr_txt">Redmi 4A</p>
+              <p class="pr_txt">Redmi 3S</p>
+              <p class="pr_validity">1-year validity</p>
+            </div>
+          </li>
+          <li class="col-sm-2">
+            <div class="pr_info">
+              <p class="price font-number"><span class="small">₹ </span>599</p>
+              <p class="pr_txt">Redmi Note 3 16G/32G</p>
+              <p class="pr_txt">Redmi 3S Prime</p>
+              <p class="pr_txt">1Redmi Note 4 32G</p>
+              <p class="pr_validity">1-year validity</p>
+            </div>
+          </li>
+          <li class="col-sm-2">
+            <div class="pr_info">
+              <p class="price font-number"><span class="small">₹ </span>749</p>
+              <p class="pr_txt">Mi Max 32G</p>
+              <p class="pr_txt">Redmi Note 4 64G</p>
+              <p class="pr_validity">1-year validity</p>
+            </div>
+          </li>
+          <li class="col-sm-2">
+            <div class="pr_info">
+              <p class="price font-number"><span class="small">₹ </span>899</p>
+              <p class="pr_txt">Mi Max Prime</p>
+              <p class="pr_validity">1-year validity</p>
+            </div>
+          </li>
+          <li class="col-sm-2">
+            <div class="pr_info">
+              <p class="price font-number"><span class="small">₹ </span>1199</p>
+              <p class="pr_txt">Mi 5</p>
+              <p class="pr_validity">1-year validity</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+<div class="main safe-guard">
+  <div class="container">
+    <div class="wrapper">
+      <div class="section_head">
+        <h3>Safeguard your Mi device against accidental / liquid damage</h3>
+        <p>Scared of dropping your device in hot liquid, or down a staircase? We understand that the repair costs are sometimes prohibitively expensive.Hence we bring you Mi Protect. Pay a nominal fee while purchasing your device and get 1-year accidental &amp; liquid damage coverage to protect your Mi device against possible damages.</p>
+        <a href="#FIXME" title="Learn More">Learn more ></a>
+      </div>
+
+      <div class="damage_list">
+        <div class="row">
+          <div class="col-sm-3">
+            <span class="damage_icon f1"></span>
+            <p>Fell in a liquid</p>
+          </div>
+          <div class="col-sm-3">
+            <span class="damage_icon f2"></span>
+            <p>Dropped in a toilet</p>
+          </div>
+          <div class="col-sm-3">
+            <span class="damage_icon f3"></span>
+            <p>Tea / coffee spilled over</p>
+          </div>
+          <div class="col-sm-3">
+            <span class="damage_icon f4"></span>
+            <p>Spoilt in rains</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-3">
+            <span class="damage_icon f21"></span>
+            <p>Fell on the ground</p>
+          </div>
+          <div class="col-sm-3">
+            <span class="damage_icon f22"></span>
+            <p>Accidently sat on it</p>
+          </div>
+          <div class="col-sm-3">
+            <span class="damage_icon f23"></span>
+            <p>Crushed by a car</p>
+          </div>
+          <div class="col-sm-3">
+            <span class="damage_icon f24"></span>
+            <p>Fell from a building</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="warranty_section">
+  <div class="container">
+    <div class="wrapper">
+      <div class="section_head">
+        <h3>Buy Mi Protect &amp; get 12 month warranty against accidents</h3>
+        <p>Buy Mi Protect along with your phone and get a 1-year free maintenance service. <br>In case of any accidental or liquid damage to your Mi phone, just call the Mi Protect customer service team at 1800 407 333 333, 1800 123 3330 or 0 8080 333 333 to explain the issue. They will send the relevant personnel to your address to check the issue and initiate the free servicing.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php require_once('footer.php'); ?>
