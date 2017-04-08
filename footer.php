14461ae871da1a34c0bea5d91630560775646e16
sushant thorat <sushant.thorat@ahytech.com>
2017-04-08 10:02:37 +0530

    support page cra

---
+++ footer.php
@@ -0,0 +1,121 @@
+<div class="service_section">
+ <div class="container">
+     <div class="row">
+       <div class="col-sm-4">
+         <a href="#FIXME" title="">
+           <div class="left_icon">
+             <img src="images/icon1.png" alt="Hassle-free" width="46px">
+           </div>
+           <div class="rt_text">
+             <h4>Hassle-free replacement</h4>
+             <span>10-day easy replacement policy on mi.com</span>
+           </div>
+         </a>
+       </div>
+       <div class="col-sm-4">
+         <a href="#FIXME" title="">
+           <div class="left_icon">
+             <img src="images/icon2.png" alt="Hassle-free">
+           </div>
+           <div class="rt_text">
+             <h4>100% secure payments</h4>
+             <span>We support Cards, Wallets, EMI and COD</span>
+           </div>
+         </a>
+       </div>
+       <div class="col-sm-4">
+         <a href="#FIXME" title="">
+           <div class="left_icon">
+             <img src="images/icon3.png" alt="Hassle-free">
+           </div>
+           <div class="rt_text">
+             <h4>Vast service network</h4>
+             <span>302 Mi service-centers across 199 cities</span>
+           </div>
+         </a>
+       </div>
+     </div>
+   </div>
+ </div>
+
+<div class="contact_section">
+  <div class="container">
+    <div class="contact_left row">
+      <div class="col-sm-6">
+        <h4>LETS STAY IN TOUCH</h4>
+        <span>Get updates on sales specials and more</span>
+      </div>
+      <div class="col-sm-6 input_email">
+        <form class="" action="index.html" method="post">
+          <input type="email" name="" value="" placeholder="Enter Emial Address">
+          <button type="submit" title="Submit"><i class="glyphicon glyphicon-menu-right"></i></button>
+        </form>
+      </div>
+    </div>
+    <div class="contact_right row">
+      <div class="col-sm-6">
+        <h4>FOLLOW MI!</h4>
+        <span>We want to hear from you</span>
+      </div>
+      <div class="col-sm-6 social_icon">
+        <a href="#FIXME" title="Twitter"><i class="fa fa-twitter"></i></a>
+        <a href="#FIXME" title="youtube"><i class="fa fa-youtube"></i></a>
+        <a href="#FIXME" title="instagram"><i class="fa fa-instagram"></i></a>
+        <a href="#FIXME" title="facebook"><i class="fa fa-facebook"></i></a>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<footer id="footer">
+<div class="container">
+  <div class="row footer_links">
+    <div class="col-sm-9 row">
+      <div class="col-sm-3">
+        <h4 class="foot-header">SUPPORT</h4>
+        <a href="#FIXME" title="Customer Service">Customer Service</a>
+        <a href="#FIXME" title="">Shipping FAQ</a>
+        <a href="#FIXME" title="Service Centres">Service Centres</a>
+        <a href="#FIXME" title="Warranty">Warranty</a>
+        <a href="#FIXME" title="Product Authentication">Product Authentication</a>
+      </div>
+      <div class="col-sm-3">
+        <h4 class="foot-header">SHOP AND LEARN</h4>
+        <a href="#FIXME" title="Mi 5">Mi 5</a>
+        <a href="#FIXME" title="Mi Max">Mi Max</a>
+        <a href="#FIXME" title="Redmi 3S">Redmi 3S</a>
+        <a href="#FIXME" title="Redmi Note 4">Redmi Note 4</a>
+        <a href="#FIXME" title="Mi Band 2 / Mi Power Bank">Mi Band 2 / Mi Power Bank</a>
+      </div>
+      <div class="col-sm-3">
+        <h4 class="foot-header">ABOUT US</h4>
+        <a href="#FIXME" title="Xiaomi">Xiaomi</a>
+        <a href="#FIXME" title="Mediakit">Mediakit</a>
+        <a href="#FIXME" title="Culture">Culture</a>
+        <a href="#FIXME" title="Privacy Policy">Privacy Policy</a>
+      </div>
+      <div class="col-sm-3">
+        <h4 class="foot-header">CONTACT US</h4>
+        <a href="#FIXME" title="Email">Email</a>
+        <a href="#FIXME" title="Careers">Careers</a>
+      </div>
+    </div>
+    <div class="col-sm-2">
+      <div class="help_contact">
+        <span class="cont_number">1800 103 6286</span>
+        <span class="cont_text">Service hours: 09:00-21:00</span>
+        <a href="#FIXME" title="CHAT SUPPORT" class="chat_box">CHAT SUPPORT</a>
+      </div>
+    </div>
+  </div>
+  <div class="footer_bottom">
+    <div class="copyright_cont">
+      <span>Copyright &copy; 2010 - 2017 Xiaomi. All Rights Reserved</span>
+    </div>
+    <div class="location_change">
+      <a href="#FIXME" title="Change Location ">Change Location <i class="glyphicon glyphicon-menu-right"></i></a>
+    </div>
+  </div>
+</div>
+</footer>
